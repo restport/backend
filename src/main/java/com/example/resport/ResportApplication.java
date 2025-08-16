f6e383fe37dfcfc9b47111b96697f0725e4b30bc
YuYoHan <zxzz8014@naver.com>
2025-08-16 23:55:30 +0900

    [Chore] 빌드 설정

---
+++ src/main/java/com/example/resport/ResportApplication.java
@@ -0,0 +1,13 @@
+package com.example.resport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResportApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/resport/ResportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.resport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
